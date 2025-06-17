07e4783cec6416098611112295b4fdd6eb42dbb8
ParthLad03 <parthlad2403@gmail.com>
2025-06-17 13:19:10 +0530

    updated layout files

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,14 +7,10 @@ export const metadata: Metadata = {
   generator: 'v0.dev',
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
